9754a8019e094a77879f524629a684b3bbaaa7f7
Laura Andrade <lauraandrad05@gmail.com>
2023-07-31 10:57:43 -0300

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+noun = input("NOUN: ")
+noun2 = input("NOUN (PLURAL): ")
+noun3 = input("NOUN: ")
+place = input("PLACE: ")
+adj = input("ADJECTIVE: ")
+noun4 = input("NOUN: ")
+
+madlib = f" Be kind to your {noun}-footed {noun2}\n For a duck may be somebody`s {noun3},\n Be kind to your {noun2} in {place}\n Where the weather is always {adj}.\n You may think that this is the {noun4},\n Well it is."
+
+print(madlib)
